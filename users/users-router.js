6a83fa93d5fce42f072d440385c768ff88675394
Danielle B <danibaxx@hotmail.com>
2020-01-20 16:43:15 -0700

    setup users-model and users-router, now to bring into index.js for the server to utilize

---
+++ users/users-router.js
@@ -0,0 +1,47 @@
+const bcrypt = require('bcryptjs');
+const express = require('express');
+const usersModel = require('./users-model');
+
+const router = express.Router()
+
+function restricted() {
+  const authErr = {
+    message: 'Cannot pass go!'
+  };
+
+  return async (req, res, next) => {
+    try {
+      const { username, password } = req.headers;
+      if (!username || !password) {
+        return res.status(401).json(authErr)
+      };
+
+      const user = await usersModel.findBy({ username }).first()
+      if (!user) {
+        return res.status(401).json(authErr)
+      };
+
+      const pwValid = await bcrypt.compare(password, user.password)
+      if (!pwValid) {
+        return res.status(401).json(authErr)
+      };
+
+      next();
+
+    } catch(err) {
+      next(err)
+    };
+  };
+};
+
+router.get('/', (req, res, next) => {
+  try {
+    const users = await usersModel.find()
+
+    res.json(users)
+  } catch(err) {
+    next(err)
+  }
+});
+
+module.exports = router;
